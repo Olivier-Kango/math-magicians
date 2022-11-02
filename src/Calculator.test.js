13d81b58fdd522502a9b0e957f555c2392275cbf
Olivier-Kango <olivierkango@gmail.com>
2022-11-02 13:50:07 -0700

    Fix linter error in test

---
+++ src/Calculator.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import Calculator from './components/Calculator.js';
+
+test('renders learn react link', () => {
+  render(<Calculator />);
+});
