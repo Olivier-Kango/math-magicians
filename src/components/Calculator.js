a64b2d00d096bf157fc489b8ca1a9332c223c2c1
Olivier-Kango <olivierkango@gmail.com>
2022-11-02 13:11:58 -0700

    Import function calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,32 @@
+import './Calculator.css';
+
+function Calculator() {
+  return (
+    <div className="Calculator">
+      <ul>
+        <li className="result">0</li>
+        <li>AC</li>
+        <li>+/-</li>
+        <li>%</li>
+        <li>÷</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>x</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>-</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>+</li>
+        <li>0</li>
+        <li>.</li>
+        <li>=</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Calculator;
