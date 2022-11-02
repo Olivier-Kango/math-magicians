3b999aaca288db10b1f01678bdde609072e3f90e
Olivier-Kango <olivierkango@gmail.com>
2022-11-02 15:21:08 -0700

    Use class based components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,41 @@
+import React from 'react';
 import './Calculator.css';
 
-function Calculator() {
-  return (
-    <div className="Calculator">
-      <ul>
-        <li className="result">0</li>
-        <li>AC</li>
-        <li>+/-</li>
-        <li>%</li>
-        <li>÷</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>x</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>-</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>+</li>
-        <li>0</li>
-        <li>.</li>
-        <li>=</li>
-      </ul>
-    </div>
-  );
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="Calculator">
+        <h1>Calculator</h1>
+        <ul>
+          <li className="result">0</li>
+          <li className="left">AC</li>
+          <li className="left">+/-</li>
+          <li className="left">%</li>
+          <li className="right">÷</li>
+          <li className="left">7</li>
+          <li className="left">8</li>
+          <li className="left">9</li>
+          <li className="right">x</li>
+          <li className="left">4</li>
+          <li className="left">5</li>
+          <li className="left">6</li>
+          <li className="right">-</li>
+          <li className="left">1</li>
+          <li className="left">2</li>
+          <li className="left">3</li>
+          <li className="right">+</li>
+          <li className="zero">0</li>
+          <li className="left">.</li>
+          <li className="right">=</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Calculator;
