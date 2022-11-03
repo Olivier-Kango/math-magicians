339183a52073ac0a8a0732b2f1269f49318c01b3
Olivier-Kango <olivierkango@gmail.com>
2022-11-03 08:55:05 -0700

    Handle click

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,33 +3,45 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { number: 0 };
   }
 
+  handleClick(e) {
+    this.setState({
+      number: e.target.innerText,
+    });
+  }
+
+  // handleTextChange(newText) {
+  //   this.setState({text: newText});
+  // }
+
   render() {
+    const { number } = this.state;
     return (
-      <ul>
-        <li className="result">0</li>
-        <li className="left">AC</li>
-        <li className="left">+/-</li>
-        <li className="left">%</li>
-        <li className="right">÷</li>
-        <li className="left">7</li>
-        <li className="left">8</li>
-        <li className="left">9</li>
-        <li className="right">x</li>
-        <li className="left">4</li>
-        <li className="left">5</li>
-        <li className="left">6</li>
-        <li className="right">-</li>
-        <li className="left">1</li>
-        <li className="left">2</li>
-        <li className="left">3</li>
-        <li className="right">+</li>
-        <li className="zero">0</li>
-        <li className="left">.</li>
-        <li className="right">=</li>
-      </ul>
+      <div>
+        <button type="button" className="result">{number}</button>
+        <button type="button" className="left">AC</button>
+        <button type="button" className="left">+/-</button>
+        <button type="button" className="left">%</button>
+        <button type="button" className="right">÷</button>
+        <button type="button" className="left">7</button>
+        <button type="button" className="left">8</button>
+        <button type="button" className="left">9</button>
+        <button type="button" className="right">x</button>
+        <button type="button" className="left">4</button>
+        <button type="button" className="left">5</button>
+        <button type="button" className="left">6</button>
+        <button type="button" className="right">-</button>
+        <button type="button" className="left" onClick={this.handleClick}>1</button>
+        <button type="button" className="left">2</button>
+        <button type="button" className="left">3</button>
+        <button type="button" className="right">+</button>
+        <button type="button" className="zero">0</button>
+        <button type="button" className="left">.</button>
+        <button type="button" className="right">=</button>
+      </div>
     );
   }
 }
