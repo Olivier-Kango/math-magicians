087878f94e8ef353ffa9578c82fa4e8b0b60bd3c
Olivier-Kango <olivierkango@gmail.com>
2022-11-02 17:00:26 -0700

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Calculator/i);
+  const linkElement = screen.getByText(/AC/i);
   expect(linkElement).toBeInTheDocument();
 });
