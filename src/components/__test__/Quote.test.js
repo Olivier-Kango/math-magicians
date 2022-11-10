6e639cdee1b9b604b9d8a0cfde242fb4f5d56fc2
Olivier-Kango <olivierkango@gmail.com>
2022-11-10 11:52:22 -0800

    Add the test to the Quote component

---
+++ src/components/__test__/Quote.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Quote from '../Quote';
+
+describe('Testing the Home page:', () => {
+  test('should render', () => {
+    render(<Quote />);
+    const textElement = screen.getByText(/Mathematics is not about numbers, equations, computations, or algorithms:/i);
+  expect(textElement).toMatchSnapshot();
+  });
+});
