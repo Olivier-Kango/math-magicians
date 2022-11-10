bd4695f24baf6d4e7c98be32dc18b749b4d992a6
Olivier-Kango <olivierkango@gmail.com>
2022-11-10 11:51:40 -0800

    Add the test to the Calculator component

--- src/components/__test__/Calculator.test.js
+++ src/components/__test__/Calculator.test.js
@@ -2,14 +2,27 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Calculator from '../Calculator';
 
-describe('<Calculator />', () => {
+describe('Testing Calculator component', () => {
+
+  // Show button numbers
   it('shows numbers', () => {
     render(<Calculator />);
        
     const buttons = document.getElementsByClassName('left');
 
     Array.from(buttons).forEach((n) => {
-      expect(screen.getByText(n.innerHTML.toString())).toBeInTheDocument();
+      expect(screen.getByText(n.innerHTML.toString())).toMatchSnapshot();
+    });
+  });
+
+  // Show calculator operators
+  it('shows operators', () => {
+    render(<Calculator />);
+  
+    const operators = document.getElementsByClassName('right');
+
+    Array.from(operators).forEach((n) => {
+      expect(screen.getByText(n.innerHTML.toString())).toMatchSnapshot();
     });
   });
 });
