8993f4895d2619990fa49e58dfbcd9bd616cbf5b
Olivier-Kango <olivierkango@gmail.com>
2022-11-09 22:30:54 -0800

    Import BrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
 );
 
 reportWebVitals();
