9e7f37b2a681b391d97033ff66918d427d8b3731
Olivier-Kango <olivierkango@gmail.com>
2022-11-09 22:34:25 -0800

    Implement and export Quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const h3 = {
+  margin: '30vh 0',
+  textAlign: 'center',
+  color: '#00004B',
+};
+
+const Quote = () => (
+  <div>
+    <h3 style={h3}>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      <br />
+      It is about understanding.
+      <br />
+      <br />
+      <i>William Paul Thurston</i>
+    </h3>
+  </div>
+);
+
+export default Quote;
