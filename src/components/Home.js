c8cbf98288ec7ab857bbf2ccfc15121e8657f1bb
Olivier-Kango <olivierkango@gmail.com>
2022-11-09 22:33:26 -0800

    Implement Home component

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const h3 = {
+  margin: '60px 0',
+};
+
+const Home = () => (
+  <div>
+    <h3 style={h3}>Welcome to our page!</h3>
+    <p>
+      In previous projects, I created the core functionality of the app:a calculator component.
+      Now I develop a full website with several pages.
+    </p>
+    <p>
+      I create a website consisting of 3 pages: Home, Calculator, and Quote.
+      To navigate through those pages, you have to click to those links above.
+    </p>
+  </div>
+);
+
+export default Home;
